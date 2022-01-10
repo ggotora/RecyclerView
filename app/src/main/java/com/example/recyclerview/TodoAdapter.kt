9913094104c6293b9a5c3c5d5facff8bc605bbf6
Gilbert Gotora <gilbertgotora2@gmail.com>
2022-01-10 05:44:01 +0200

    Add TodoAdapter

---
+++ app/src/main/java/com/example/recyclerview/TodoAdapter.kt
@@ -0,0 +1,8 @@
+package com.example.recyclerview
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class TodoAdapter {
+    inner class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
